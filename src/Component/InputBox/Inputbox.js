ad99b32c13d55e7dbe42539e20995758ba70cd99
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-06-23 08:50:16 +0530

    Add-Product

---
+++ src/Component/InputBox/Inputbox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Inputbox(props) {
+    return (+
+
+
+        
+        <div>
+            
+        </div>
+    );
+}
+
+export default Inputbox;
---
+++ src/Component/InputBox/inputbox.style.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function inputboxstyle(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default inputboxstyle;
