c5632c2e405290fed4a3f4d43532c268e0f239ae
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-09-13 08:43:58 +0530

    Product-catagory

--- src/Container/Product/Product.js
+++ src/Container/Product/Product.js
@@ -30,23 +30,24 @@ function Product(props) {
     const [manufacturer, setManufacturer] = useState('');
     const [price, setPrice] = useState('');
     const [data, setData] = useState([]);
-
-    const [search , setSearch] = useState({
-        catagory : "All",
-        searchBar : ""
-    });
+    const [search , setSearch] = useState("All");
 
     console.log(search);
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+
     const handleCatagory = (c) => {
-   setSearch({...search , catagory : c})
+//    setSearch({...search , catagory : c})
+
+   let search = product.filter((f) => (
+    f.id.toString().includes(f) ||
+    f.product_list.toString().includes(f)   
+
+))
+
+console.log(search);
+
     };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+
 
 useEffect(() =>{
    dispatch(getdocdata());
@@ -147,8 +148,10 @@ useEffect(() =>{
                                                             <img  src={e.url}/>
                                                         </div>
                                                         <div className="detail-box">
+                                                            <div className='pro-box-t'>
                                                                 <h5 className='pro-name'>{e.product_name}</h5>
                                                                 <h6>Price : {e.product_price}</h6>
+                                                            </div>    
                                                                 <p className='pro-type'>Catagory : {e.product_list}</p>
                                                                 <p className='description-pro'>{e.product_description}</p>
                                                         </div>
@@ -157,313 +160,13 @@ useEffect(() =>{
 
                                         ))
                                     }
-                        
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Men's Shirt
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p1.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Men's Shirt
-                                    </h5>
-                                    <h6>
-                                        $75
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p2.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Men's Shirt
-                                    </h5>
-                                    <h6>
-                                        $80
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p3.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Women's Dress
-                                    </h5>
-                                    <h6>
-                                        $68
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p4.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Women's Dress
-                                    </h5>
-                                    <h6>
-                                        $70
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p5.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Women's Dress
-                                    </h5>
-                                    <h6>
-                                        $75
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p6.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Women's Dress
-                                    </h5>
-                                    <h6>
-                                        $58
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p7.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Women's Dress
-                                    </h5>
-                                    <h6>
-                                        $80
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p8.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Men's Shirt
-                                    </h5>
-                                    <h6>
-                                        $65
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p9.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Men's Shirt
-                                    </h5>
-                                    <h6>
-                                        $65
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p10.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Men's Shirt
-                                    </h5>
-                                    <h6>
-                                        $65
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p11.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Men's Shirt
-                                    </h5>
-                                    <h6>
-                                        $65
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6 col-md-4 col-lg-3">
-                            <div className="box">
-                                <div className="option_container">
-                                    <div className="options">
-                                        <a href className="option1">
-                                            Add To Cart
-                                        </a>
-                                        <a href className="option2">
-                                            Buy Now
-                                        </a>
-                                    </div>
-                                </div>
-                                <div className="img-box">
-                                    <img src="images/p12.png" alt />
-                                </div>
-                                <div className="detail-box">
-                                    <h5>
-                                        Women's Dress
-                                    </h5>
-                                    <h6>
-                                        $65
-                                    </h6>
-                                </div>
-                            </div>
-                        </div>
+                       
                     </div>
-                    <div className="btn-box">
+                    {/* <div className="btn-box">
                         <a href>
                             View All products
                         </a>
-                    </div>
+                    </div> */}
 
                     
                 </div>
